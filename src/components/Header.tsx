390d60450229bff08fdb23ab103e5f855216ce2c
sevenlinksglasses <sevenlinksglasses@gmail.com>
2026-09-02 04:13:52 -0700

    Updated website

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
     { label: 'About', href: '#about' },
     { label: 'Services', href: '#services' },
     { label: 'Gallery', href: '#gallery' },
-    { label: ' my Contact', href: '#contact' },
+    { label: 'Contact', href: '#contact' },
   ];
 
   const scrollToSection = (href: string) => {
